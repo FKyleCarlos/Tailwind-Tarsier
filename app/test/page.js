eb86c5f4901f416a7ffe8c3c1c12c2c1250e4c9d
FKyleCarlos <francis.kylecarlos@gmail.com>
2026-01-24 15:46:44 -0700

    added result checker on the test page. To be moved later for smooth transition

--- app/test/page.js
+++ app/test/page.js
@@ -1,7 +1,38 @@
+"use client"
 import Header from "@/app/components/header";
+import { useState } from "react";
 
-export default function Page() {
+export default function Page({ inputAnswer = "", solution = [] }) {
+  const [answerThresh, setAnswerThresh] = useState();
+  const inputArray = inputAnswer.split(' ');
+  
+  async function ResultAnalyzer(){
+    let counter = 0;
+    let finalScore = 0;
+
+    solution.forEach(solution => {
+      const solutionWords = solution.split(' ');
+      inputArray.forEach(inWord => {
+        solutionWords.forEach(solWord => {
+          if(inWord === solWord){
+            counter++;
+          }
+        });
+      });
+        console.log(counter);
+        if(counter > finalScore){
+          finalScore = counter;
+        }
+        counter = 0;
+    });
+    
+    setAnswerThresh((finalScore/inputArray.length)*100);
+  } 
+
   return (
-    <Header />
+    <div>
+      <button onClick={ResultAnalyzer} className="p-2 bg-slate-500">Click</button>
+      <p>Your answer was {answerThresh}% correct</p>
+    </div>
   )
 }
