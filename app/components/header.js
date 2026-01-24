424f30a604b782c89b3387384e14e202f6df7252
camriegersait <183440540+camriegersait@users.noreply.github.com>
2026-01-24 14:13:14 -0700

    updated description component

--- app/components/header.js
+++ app/components/header.js
@@ -1,10 +1,10 @@
 import NavBlock from "@/app/components/navBlock";
 
-export default function Header() {
+export default function Header({title}) {
   return (
     <header className="flex rounded-4xl w-auto sm:h-1/2 md:h-full bg-amber-600">
       <h1 className="flex-3 flex items-center justify-center text-4xl sm:test-2xl">
-        Header
+        {title}
       </h1>
       <NavBlock/>
     </header>
--- app/components/problemDescription.js
+++ app/components/problemDescription.js
@@ -1,6 +1,6 @@
 export default function ProblemDescription({description}) {
   return (
-    <div className="bg-slate-200 p-2 text-black">
+    <div className="bg-slate-200 mt-4 p-2 rounded-lg text-black h-full">
       {description}
     </div>
   )
