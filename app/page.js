242d0cb7bfe2ed3a3a7818404868e73fcbc5ac6e
Noah Frank <noahdwfrank@gmail.com>
2026-01-24 16:39:08 -0700

    Added firebase API integration

--- app/page.js
+++ app/page.js
@@ -3,16 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="grid grid-cols-2 h-screen w-full">
-          <div className="bg-[#B87A00]">
-            Problem
-          </div>
-          <div className="bg-[#FFF1D6] p-10">
-            Render
-            <div className="bg-[#FFCF70] h-full">
-              asd
-            </div>
-          </div>
+      <main className="flex flex-col h-screen w-full">
+
       </main>
     </div>
   );
