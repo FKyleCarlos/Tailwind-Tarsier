ddf65d5cf34bd276d7ef5b60ef6042b40eb83aa7
FKyleCarlos <francis.kylecarlos@gmail.com>
2026-01-25 10:46:38 -0700

    Added problem 2

--- app/levels/problem2.js
+++ app/levels/problem2.js
@@ -1,8 +1,19 @@
+import Image from "next/image";
+import Tarsier from "../../public/tarsier.png";
+import Bug from "../../public/bedbug.svg"
 
-export default function Problem2(){
-    return(
-        <div>
-            
+export default function Problem2({ answer="justify-center" }){
+    return (
+        <div className="flex justify-center h-full w-full bg-indigo-950">
+            <div className={`flex flex-1 h-full w-full items-center ${answer}`}>
+                <Image 
+                src={Tarsier} 
+                alt='tarsier' 
+                height={150}/>
+            </div>
+            <div className="flex flex-1 h-full w-full items-center justify-start">
+                <Image src={Bug} alt='bug' height={70}/>
+            </div>
         </div>
     )
 }
